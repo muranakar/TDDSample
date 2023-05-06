9ca272ce813cc58e5cd705884a9d221b7c528c3e
muranakar <p1211065@gmail.com>
2023-05-06 09:12:31 +0900

    Initial Commit

---
+++ TDDSample/TDDSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDSampleApp.swift
+//  TDDSample
+//
+//  Created by 村中令 on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct TDDSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
